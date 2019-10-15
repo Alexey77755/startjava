a7c6016f78b1ae4bf6de16ff205ca85fbf87ddeb
Alex <Alex_TNK_BP@mail.ru>
2019-10-15 22:05:29 +0300

    First commit

---
+++ Lesson 1/ConditionalStatement.java
@@ -0,0 +1,32 @@
+public class MyFirstApp {
+    public static void main(String[] args) {
+        int age = 0;
+        boolean maleGender = true;
+        float height = 1.0;
+        String name = "Alexey";
+        char c = str.charAt(0);
+
+        if (age > 20) {
+            System.out.println("Ok");
+        }
+        if (maleGender) {
+            System.out.println("Ok");
+        }
+        if (maleGender == false) {
+            System.out.println("Super");
+        }
+        if (height < 1.80) {
+            System.out.println("dwarf");
+        } else {
+            System.out.println("giant");
+        }
+        if (c == 'M') {
+            System.out.println("MMM");
+        } else if (c == 'I') {
+            System.out.println("III");
+        } else {
+            System.out.println("RRR");
+        }
+
+    }
+}
---
+++ Lesson 1/Variable.java
@@ -0,0 +1,21 @@
+class Variable {
+    public static void main(String[] args) {
+        byte core = 2;
+        short coreSpeed = 2000;
+        int technology = 65;
+        long family = 6;
+        float busSpeed = 199.99;
+        double multiplier = 10.0;
+        сhar model = 'F';
+        boolean isIntel = true;
+
+        System.out.println("core " + core);
+        System.out.println("coreSpeed " + coreSpeed);
+        System.out.println("technology " + technology);
+        System.out.println("family " + family);
+        System.out.println("busSpeed " + busSpeed);
+        System.out.println("multiplier " + multiplier);
+        System.out.println("model " + model);
+        System.out.println("intel " + intel);
+    }
+}
