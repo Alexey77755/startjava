fbb229916ebb62644e6a8e1aacd66910125581bf
Alex <Alex_TNK_BP@mail.ru>
2020-01-08 18:49:58 +0300

    Unicode

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -19,33 +19,33 @@ public class Calculator {
         } else {
             System.out.println("Похоже, вы ввели не целое число. Введите снова.");
         }
+
         System.out.print ("Введите знак математической операции ");
-        char s=' ';
+        char sign;
         if (sc.hasNextLine()) {
-            s = sc.next().charAt(0);
+            sign = sc.next().charAt(0);
         } else {
             System.out.println("Похоже, вы ввели не верный знак. Введите снова.");
         }
 
-        if (s == '+') {
+        if (sign == '+') {
             System.out.println(num1 + num2);
-        } else if(s == '-') {
+        } else if(sign == '-') {
             System.out.println(num1 - num2);
-        } else if(s == '*') {
+        } else if(sign == '*') {
             System.out.println(num1 * num2);
-        } else if(s == '/') {
+        } else if(sign == '/') {
             System.out.println(num1 / num2);
-        } else if(s == '^') {
+        } else if(sign == '^') {
             int result = 1;
             for (int i = 1; i <= num2; i++) {
-                result*= num1;
+                result *= num1;
             }
             System.out.println(result);
-        } else if(s == '%') {
+        } else if(sign == '%') {
             System.out.println(num1 % num2);
         } else {
             System.out.println("Введеный знак не соответстует знаку математической операции. Введите снова.");
         }
-        sc.close();
       }
     }
---
+++ Lesson 1/Unicode.java
@@ -0,0 +1,9 @@
+
+public class Unicode {
+    public static void main(String[] args) {
+    char number;
+    for(int i = 33;i <= 126;i++) {
+        number = (char)i;
+        System.out.println(number);
+    }
+}
