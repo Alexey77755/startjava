1e3298acfb41e247735e144929537658ce155dca
Alex <Alex_TNK_BP@mail.ru>
2020-01-20 22:08:53 +0300

    Class_Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,30 @@
+
+public class Person {
+
+	boolean isSex = 0;
+    String name = "Alex";
+    float height = 1.75;
+    float weight = 70.6;
+    int age = 30;
+
+    void go(){
+
+    }
+
+    void sit(){
+
+    }
+
+    void toRun(){
+
+    }
+
+    void speak(){
+
+    }
+
+    void learnJava(){
+
+    }
+
+}
