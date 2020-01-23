2265388ccf6379ffed9cf9236c1803fc343b3fca
Alex <Alex_TNK_BP@mail.ru>
2020-01-23 21:21:20 +0300

    Wolf and WolfTest modify

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,29 +1,66 @@
 
 public class Wolf {
 
-    boolean isSex = true;
-    String nickname = "Bob";
-    float weight = 70.6f;
-    int age = 5;
-    String color = "black";
+    private boolean isSex = true;
+    private String nickname = "Bob";
+    private float weight = 70.6f;
+    private int age = 5;
+    private String color = "black";
 
-    void go() {
-
-    }
-
-    void sit() {
+    public void go() {
 
     }
 
-    void run() {
+    public void sit() {
 
     }
 
-    void howl() {
+    public void run() {
 
     }
 
-    void hunt() {
+    public void howl() {
 
+    }
+
+    public void hunt() {
+
+    }
+
+    public boolean getSex() {
+        return isSex;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public float getWeight() {
+        return weight;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void setSex(boolean sex){
+        isSex=sex;
+    }
+    public void setNickname(String nname){
+        nickname=nname;
+    }
+    public void setWeight(float w){
+        weight=w;
+    }
+    public void setAge(int a){
+        if(a>8) {
+            System.out.println("Некорректный возраст");
+        }
+        else {
+            age=a;
+        }
+    }
+    public void setColor(String color1){
+       color=color1;
     }
 }
--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -2,22 +2,22 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf1 = new Wolf();
-        wolf1.isSex = false;
-        wolf1.nickname = "Tim";
-        wolf1.weight = 50.1f;
-        wolf1.age = 3;
-        wolf1.color = "Gray";
+        wolf1.setSex(false);
+        wolf1.setNickname("Tim");
+        wolf1.setWeight(50.1f);
+        wolf1.setAge(3);
+        wolf1.setColor("Gray");
 
-        System.out.println(wolf1.isSex);
-        System.out.println(wolf1.nickname);
-        System.out.println(wolf1.weight);
-        System.out.println(wolf1.age);
-        System.out.println(wolf1.color);
+        System.out.println(wolf1.getSex());
+        System.out.println(wolf1.getNickname());
+        System.out.println(wolf1.getWeight());
+        System.out.println(wolf1.getAge());
+        System.out.println(wolf1.getColor());
 
-         wolf1.go();
-         wolf1.sit();
-         wolf1.run();
-         wolf1.howl();
-         wolf1.hunt();
+        wolf1.go();
+        wolf1.sit();
+        wolf1.run();
+        wolf1.howl();
+        wolf1.hunt();
     }
 }
